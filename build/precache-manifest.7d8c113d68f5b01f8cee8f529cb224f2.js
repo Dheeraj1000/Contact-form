ecff4bb3aa95896dd1f753583ccd227adf1b824d
Dheeraj1000 <93862893+Dheeraj1000@users.noreply.github.com>
2023-04-07 22:00:48 +0530

    first commit

---
+++ build/precache-manifest.7d8c113d68f5b01f8cee8f529cb224f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bd72b3d7895e52bbb9b1",
+    "url": "/static/css/main.d827d391.chunk.css"
+  },
+  {
+    "revision": "bd72b3d7895e52bbb9b1",
+    "url": "/static/js/main.bd72b3d7.chunk.js"
+  },
+  {
+    "revision": "d1ec89f2627e3432393e",
+    "url": "/static/js/1.d1ec89f2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b0858d00b6da39823a2e1b579f4d5482",
+    "url": "/index.html"
+  }
+];
